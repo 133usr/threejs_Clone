7eca50a6e4d40a9c4ba746e3ed345340a01c0e25
133usr <78338503+133usr@users.noreply.github.com>
2023-05-01 13:50:16 +0530

    asd

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import './style.css'
+import '../src/style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
